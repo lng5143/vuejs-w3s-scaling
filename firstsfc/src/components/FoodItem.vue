1c6c484fec2c7430785a004f6a7d5783dbd0d23e
lamnguyen <nguyenphuonglam96@gmail.com>
2023-08-19 14:46:35 +0700

    first sfc page + FoodItem.vue component

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,31 @@
+<template> 
+    <div v-on:click="countClicks">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+      <p id="red">You have clicked me {{ count }} times.</p>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          name: 'Apples',
+          message: 'I like apples',
+          count: 0
+        }
+      },
+      methods: {
+        countClicks() {
+          this.count = this.count + 1;
+        }
+      }
+    }
+  </script>
+  
+  <style>
+    #red {
+      font-weight: bold ;
+      color: rgb(144, 12, 12);
+    }
+  </style>
